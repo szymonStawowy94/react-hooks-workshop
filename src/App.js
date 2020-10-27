5f891f64a159edd2bb41988ac8c6b7a93e4912de
Szymon <s.stawowy@pleodigital.com>
2020-10-27 19:26:07 +0100

    useRef hook example

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 import './App.css';
 import {countries} from "./countries"
 
 const App = () => {
-	const [countryList, setCoutryList] = useState(countries); // [state, setState]
-	const [inputValue, setInputValue] = useState("");
+	const myBox = useRef(null);
 
-	const handleInputChange = e => {
-		setInputValue(e.target.value);
-		setCoutryList(countryList.filter(country => country.toLowerCase().includes(e.target.value.toLowerCase())))
-		if(!e.target.value.length) {
-			setCoutryList(countries)
-		}
+	console.log(myBox)
+
+	const changeBoxColor = () => {
+		myBox.current.style.background = "blue";
 	}
 	return (
 		<div className="App">
-			<label for="country">Search country</label>
-			<input
-				value={inputValue}
-				onChange={handleInputChange}
-				name="country"
-				id="country"/>
-			<ul>
-				{countryList.map(country => (
-					<li>{country}</li>
-				))}
-			</ul>
+			<div className="box" ref={myBox}/>
+			<button onClick={changeBoxColor}>change color</button>
 		</div>
 	);
 }
